34c04a1f75ea53c3a4582c057e2f5ceb8e4ebe28
arnavgupta9000 <56313607+arnavgupta9000@users.noreply.github.com>
2024-02-18 22:31:47 -0700

    Update q7.sql

--- Q7/q7.sql
+++ Q7/q7.sql
@@ -1,1 +1,1 @@
-select book_id, title, pyear from(select b.book_id, b.title, b.pyear, rank() over (order by count(*) desc) as book_rank from borrowings bor join books b on bor.book_id = b.book_id group by b.book_id, b.title, b.pyear) as rank where book_rank <= 3;
+select book_id, title, pyear from(select bo.book_id, bo.title, bo.pyear, rank() over (order by count(*) desc) as book_rank from borrowings b join books bo on b.book_id = bo.book_id group by bo.book_id, bo.title, bo.pyear) as rank where book_rank <= 3;
