63791f96170b45b52dd4931986690286eabd0a52
arnavgupta9000 <56313607+arnavgupta9000@users.noreply.github.com>
2024-02-18 22:31:04 -0700

    Update q3.sql

--- Q3/q3.sql
+++ Q3/q3.sql
@@ -1,1 +1,1 @@
-select distinct bo.book_id, title, avg(rating) as aver from reviews r, books bo where r.book_id = bo.book_id group by bo.book_id, title having count(*) >=2 order by aver desc limit 3;
+select distinct bid, email from members m, borrowings b, books bo left join waitlists w on b.book_id = w.book_id where b.end_date is not null and m.email = b.member and bo.book_id = b.book_id and julianday(b.end_date) - julianday(b.start_date) > 14 and (w.priority < 5 or w.priority is null);
