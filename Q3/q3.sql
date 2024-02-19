b0fd0f9426f0f1273b25a4ae291baa4ff13c90f8
arnavgupta9000 <56313607+arnavgupta9000@users.noreply.github.com>
2024-02-18 22:30:26 -0700

    Update q3.sql

--- Q3/q3.sql
+++ Q3/q3.sql
@@ -1,1 +1,1 @@
- select distinct bid, email from members m, borrowings b, waitlists w, books bo where end_date not null and m.email = b.member and julianday(b.end_date) - julianday(b.start_date) > 14 and (priority < 5 or priority is null)  and bo.book_id = b.book_id;
+select distinct bid, email from members m, borrowings b, books bo left join waitlists w on b.book_id = w.book_id where b.end_date is not null and m.email = b.member and bo.book_id = b.book_id and julianday(b.end_date) - julianday(b.start_date) > 14 and (w.priority < 5 or w.priority is null);
