491d090517bc73deb82673762a380df65fd09b74
arnavgupta9000 <56313607+arnavgupta9000@users.noreply.github.com>
2024-02-18 22:30:12 -0700

    Update q2.sql

--- Q2/q2.sql
+++ Q2/q2.sql
@@ -1,1 +1,1 @@
-select distinct bid, email, end_date from borrowings b, members m, books bo where m.faculty = 'CS' and (lower(bo.author) like '%john%' or lower(bo.author) like '%marry%') and b.book_id = bo.book_id and b.member = m.email;
+select distinct b1.bid, m.email, b1.end_date  from borrowings b1 join members m on b1.member = m.email join books bo on b1.book_id = bo.book_id join borrowings b2 on b1.book_id = b2.book_id join members m2 on b2.member = m2.email where (lower(bo.author) like '%john%' or lower(bo.author) like '%marry%') and m2.faculty = 'CS'; 
