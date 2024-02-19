82fe24f4edb8626538532cdb3eff78e299594a24
arnavgupta9000 <56313607+arnavgupta9000@users.noreply.github.com>
2024-02-18 22:30:35 -0700

    Update q3.sql

--- Q3/q3.sql
+++ Q3/q3.sql
@@ -1,1 +1,1 @@
-select distinct bid, email from members m, borrowings b, books bo left join waitlists w on b.book_id = w.book_id where b.end_date is not null and m.email = b.member and bo.book_id = b.book_id and julianday(b.end_date) - julianday(b.start_date) > 14 and (w.priority < 5 or w.priority is null);
+select distinct bo.book_id, title, author,  (select count(*)from borrowings b where b.book_id = bo.book_id) as borrow, ifnull((select max(b.start_date) from borrowings b where b.book_id = bo.book_id), null) as start from books bo, borrowings b where pyear > 2001;
